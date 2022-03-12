59b3307c97986a28feebafb205937bc46495aa88
MantasBr <mantasbr4@gmail.com>
2022-03-12 22:17:23 +0200

    removed broken test

--- src/test/java/com/mantasb/BankSystem/TransactionControllerTests.java
+++ src/test/java/com/mantasb/BankSystem/TransactionControllerTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -22,6 +23,12 @@ public class TransactionControllerTests {
     private TransactionService transactionService;
     @MockBean
     private TransactionRepository transactionRepository;
+
+    @Test
+    void testFileWithNoParams() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.multipart("/transaction/import"))
+                .andExpect(MockMvcResultMatchers.status().is(400));
+    }
 
     @Test
     void testGetBalanceWithNoParams() throws Exception {
