333f22316b6ff8da45b020fee9f70294cfe22f2e
MantasBr <mantasbr4@gmail.com>
2022-03-12 18:34:02 +0200

    Changed broken test and added another one

--- src/test/java/com/mantasb/BankSystem/TransactionControllerTests.java
+++ src/test/java/com/mantasb/BankSystem/TransactionControllerTests.java
@@ -8,11 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import static org.mockito.Mockito.when;
 
 @WebMvcTest(controllers = TransactionController.class)
 public class TransactionControllerTests {
@@ -27,14 +24,14 @@ public class TransactionControllerTests {
     private TransactionRepository transactionRepository;
 
     @Test
+    void testGetBalanceWithNoParams() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/transaction/getBalance")).andExpect(MockMvcResultMatchers.status().is(400));
+    }
+
+    @Test
     void testAccountBalanceWithNoDateParams() throws Exception {
-        when(transactionService.getAccountBalance("TEST_ID1", null, null)).thenReturn(200.2);
-
-        MvcResult result = mvc.perform(MockMvcRequestBuilders.get("/transaction/getBalance")
+        mvc.perform(MockMvcRequestBuilders.get("/transaction/getBalance")
                         .param("accountNumber", "TEST_ID1"))
-                .andExpect(MockMvcResultMatchers.status().is(200))
-                .andReturn();
-
-        assert result.getResponse().getContentAsString().equals("200.2") == false;
+                .andExpect(MockMvcResultMatchers.status().is(200));
     }
 }
